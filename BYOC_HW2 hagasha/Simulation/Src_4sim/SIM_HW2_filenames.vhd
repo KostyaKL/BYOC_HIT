e38ddeefba8ff154d363d20364196e4e79449fa8
Kostya <klokshin@gmail.com>
2019-05-02 12:56:51 +0300

    HW2 init

---
+++ BYOC_HW2 hagasha/Simulation/Src_4sim/SIM_HW2_filenames.vhd
@@ -0,0 +1,24 @@
+----------------------------------------------------------------------------------
+--
+-- 
+-- This package includes definition of filenames used for simulations
+--
+--
+----------------------------------------------------------------------------------
+library   ieee;
+
+
+package BYOC_filenames is
+	
+use  ieee.std_logic_1164.all;
+use  ieee.std_logic_arith.all;  
+use  ieee.std_logic_unsigned.all;
+use  std.textio.all;
+use  IEEE.NUMERIC_STD.all;
+
+constant   program_in_file_name   : string :=  "C:\Users\8lab305\Downloads\HW2__for_students - 2019\HW2__for_students - 2019\HW2_Src_for_sim_for_students-empty\SIM_HW2_program.dat";
+constant   data_in_file_name 	  : string :=  "C:\Users\8lab305\Downloads\HW2__for_students - 2019\HW2__for_students - 2019\HW2_Src_for_sim_for_students-empty\SIM_HW2_TB_data.dat";
+
+signal 	   no_hold				  : STD_LOGIC := '0'; -- MUST be '0' for correct TB messages !!!
+
+end  BYOC_filenames;
